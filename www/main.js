628edac6e2956d13aa23f9c2d51930d6270bd356
Jeffrey McAteer <jeffrey@jmcateer.com>
2026-07-04 17:46:46 -0400

    beginning of github pages deployment, to support documentation for non-developers & a screenshot gallery

---
+++ www/main.js
@@ -0,0 +1,7 @@
+
+
+const REPO_CONTENTS_URL = "https://api.github.com/repos/Jeffrey-P-McAteer/av-switchyard/git/trees/trunk?recursive=1";
+
+
+console.log('todo fetch and parse ', REPO_CONTENTS_URL)
+
