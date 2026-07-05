8c9a5c81a109a290967b616d32a083825850522b
Jeffrey McAteer <jeffrey@jmcateer.com>
2026-07-05 10:41:03 -0400

    webpage++

--- www/main.js
+++ www/main.js
@@ -1,7 +1,1 @@
 
-
-const REPO_CONTENTS_URL = "https://api.github.com/repos/Jeffrey-P-McAteer/av-switchyard/git/trees/trunk?recursive=1";
-
-
-console.log('todo fetch and parse ', REPO_CONTENTS_URL)
-
